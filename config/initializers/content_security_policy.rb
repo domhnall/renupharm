dd1dbd9dfc2cd90b38d9d2ed0d5cf3bc1a41d1a7
Domhnall <domhnall.murphy@examtime.com>
2018-08-12 22:39:37 +0100

    Incorporating yarn, webpacker and vue.js

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,6 +11,8 @@
 #   policy.object_src  :none
 #   policy.script_src  :self, :https
 #   policy.style_src   :self, :https
+# Added by DM based on comment from webpacker install
+#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
