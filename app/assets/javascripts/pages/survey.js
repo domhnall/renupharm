82844c534207bf0f22efde4e7daa2e684124c103
Domhnall <domhnall.murphy@examtime.com>
2018-09-06 22:08:27 +0100

    Some tidy up on the recaptcha stuff to work with turbolinks.

--- app/assets/javascripts/pages/survey.js
+++ app/assets/javascripts/pages/survey.js
@@ -3,3 +3,15 @@ var recaptcha_successful = function(){
   submit_btn.disabled = false;
   submit_btn.setAttribute("aria-disabled", false);
 };
+
+
+var onload_recaptcha = function(){
+  var container = document.getElementById('g-recaptcha-container');
+  container.innerHTML = '';
+  var recaptcha = document.createElement('div');
+  grecaptcha.render(recaptcha, {
+    sitekey: container.dataset.sitekey,
+    callback: 'recaptcha_successful'
+  });
+  container.appendChild(recaptcha);
+};
