0a724ef4f90246b9de35c417e19e960329156377
Domhnall <domhnall.murphy@examtime.com>
2018-09-10 08:39:21 +0100

    Adding support for the rester interest function.

---
+++ app/controllers/sales/contacts_controller.rb
@@ -0,0 +1,22 @@
+class Sales::ContactsController < ApplicationController
+  def create
+    if @sales_contact = Sales::Contact.find_by_email(sales_contact_params[:email])
+      @sales_contact.update_attributes(sales_contact_params)
+      redirect_to(root_path, flash: {info: I18n.t("pages.index.register_interest.already_exists")}) && return
+    end
+
+    @sales_contact = Sales::Contact.new(sales_contact_params)
+    if @sales_contact.valid?
+      @sales_contact.save!
+      redirect_to root_path, flash: { success: I18n.t("pages.index.register_interest.success") }
+    else
+      redirect_to root_path, flash: { error: I18n.t("pages.index.register_interest.error").html_safe }
+    end
+  end
+
+  private
+
+  def sales_contact_params
+    params.require(:sales_contact).permit(:first_name, :surname, :email)
+  end
+end
---
+++ spec/controllers/sales/contacts_controller_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+describe Sales::ContactsController do
+  before :all do
+    @sales_contact_params = {
+      sales_contact: {
+        first_name: "Domhnall",
+        surname: "Murphy",
+        email: "domhnallmurphy@hotmail.com"
+      }
+    }
+  end
+
+  describe "#create" do
+    describe "when successful" do
+      it "should create a new Sales::Contact record" do
+        orig_count = Sales::Contact.count
+        post :create, params: @sales_contact_params
+        expect(Sales::Contact.count).to eq orig_count+1
+      end
+
+      it "should redirect the user back to the root page" do
+        post :create, params: @sales_contact_params
+        expect(response).to redirect_to root_path
+      end
+
+      it "should set an appropriate flash message" do
+        post :create, params: @sales_contact_params
+        expect(flash[:success]).to eq I18n.t("pages.index.register_interest.success")
+      end
+    end
+
+    describe "when one of the required fields is absent" do
+      before :each do
+        @revised_contact_params = {
+          sales_contact: {
+            first_name: nil,
+            surname: "Murphy",
+            email: "domhnallmurphy@hotmail.com"
+          }
+        }
+      end
+
+      it "should not create a new Sales::Contact record" do
+        orig_count = Sales::Contact.count
+        post :create, params: @revised_contact_params
+        expect(Sales::Contact.count).to eq orig_count
+      end
+
+      it "should redirect the user back to the root page" do
+        post :create, params: @revised_contact_params
+        expect(response).to redirect_to root_path
+      end
+
+      it "should set an appropriate flash message" do
+        post :create, params: @revised_contact_params
+        expect(flash[:error]).to eq I18n.t("pages.index.register_interest.error")
+      end
+    end
+
+    describe "when the contact already exists" do
+      before :all do
+        @existing_contact = Sales::Contact.create!(@sales_contact_params[:sales_contact])
+      end
+
+      it "should not create a new Sales::Contact record" do
+        orig_count = Sales::Contact.count
+        post :create, params: @sales_contact_params
+        expect(Sales::Contact.count).to eq orig_count
+      end
+
+      it "should redirect the user back to the root page" do
+        post :create, params: @sales_contact_params
+        expect(response).to redirect_to root_path
+      end
+
+      it "should set an appropriate flash message" do
+        post :create, params: @sales_contact_params
+        expect(flash[:info]).to eq I18n.t("pages.index.register_interest.already_exists")
+      end
+    end
+  end
+end
