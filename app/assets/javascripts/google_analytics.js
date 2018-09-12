c5ec76cb471463af7106aaad9533b1dc98554c76
Domhnall <domhnall.murphy@examtime.com>
2018-09-12 13:26:18 +0100

    Tidy up GoogleAnalytics tracking
    Had accidentally transposed page_title and page_path.

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,8 +1,8 @@
 document.addEventListener("turbolinks:load", function(event){
   if(typeof gtag === "function"){
     gtag('config', 'UA-125632968-1', {
-      'page_title' : event.target.location.pathname,
-      'page_path': event.target.title
+      'page_path' : event.target.location.pathname,
+      'page_title': event.target.title
     });
   }
 });
