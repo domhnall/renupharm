3e907cc9facc6f930fd43d8f1a98554b56bcac57
Domhnall <domhnall.murphy@examtime.com>
2018-09-15 11:52:57 +0100

    Only run sentry in production.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,5 @@
-Raven.configure do |config|
-  config.dsn = Rails.application.credentials.sentry[:dsn]
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = Rails.application.credentials.sentry[:dsn]
+  end
 end
