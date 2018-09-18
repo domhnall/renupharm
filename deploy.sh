8dac854d6065893bb6e4fd3b730ddf55b4d5a63b
Domhnall <domhnall.murphy@examtime.com>
2018-09-18 17:54:41 +0100

    Update deploy script to automatically authenticate between ECS and docker

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+eval "$(aws ecr get-login --no-include-email)"
 docker-compose build app
 docker tag renupharm_app 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
 docker push 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
