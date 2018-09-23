1d36454b97f627ed1891be2f466c0ecce6f27f64
Domhnall <domhnall.murphy@examtime.com>
2018-09-23 20:37:49 +0100

    Update deploy script to reference renupharm-new cluster config

--- deploy.sh
+++ deploy.sh
@@ -23,4 +23,4 @@ docker tag renupharm_app 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
 docker push 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
 
 # Deploy the updated image
-ecs-cli compose --cluster-config renupharm --file docker-compose.production.yml up
+ecs-cli compose --cluster-config renupharm-new --file docker-compose.production.yml up
