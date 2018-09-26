ae5491b6700e488db73d5df9a5e6115ad3f86d38
Domhnall <domhnall.murphy@examtime.com>
2018-09-26 09:33:39 +0100

    Updated deploy script to run full test suite before deploy.

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,23 @@ eval "$(aws ecr get-login --no-include-email)"
 
 # Clone repo and checkout branch/tag to build
 cd /tmp
-rm -rf renupharm
+sudo rm -rf renupharm
 git clone git@bitbucket.org:domhnall_murphy/renupharm.git
 cd renupharm
 git checkout $1
 cp ${PROJ_DIR}/config/master.key ./config/master.key
 
-# Build and tag app image, push image to ECR
-docker-compose build app
-docker tag renupharm_app 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
-docker push 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
+# Run full test suite
+if docker-compose run -e "RAILS_ENV=test" app bundle exec rake all_tests; then
+  echo "TEST SUITE PASSED. PROCEEDING WITH DEPLOY"
 
-# Deploy the updated image
-ecs-cli compose --cluster-config renupharm-new --file docker-compose.production.yml up
+  # Build and tag app image, push image to ECR
+  docker-compose build app
+  docker tag renupharm_app 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
+  docker push 348231524911.dkr.ecr.eu-west-1.amazonaws.com/renupharm
+
+  # Deploy the updated image
+  ecs-cli compose --cluster-config renupharm-new --file docker-compose.production.yml up
+else
+  echo "TEST SUITE FAILED. ABORTING DEPLOY" && exit 1
+fi
