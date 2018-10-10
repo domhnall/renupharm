370306dc3a02427faaa927ad580a9cf6856df2a5
Domhnall <domhnall.murphy@examtime.com>
2018-10-10 14:13:18 +0100

    Swith Comments back to ES5 to work on IE11.
    We should look at including Babel so that we can continue to write ES6
    and be confident that things will continue to work in IE11.

--- app/assets/javascripts/dashboard/admin/shared/comments.js
+++ app/assets/javascripts/dashboard/admin/shared/comments.js
@@ -1,32 +1,38 @@
-const Comments = (function(){
+var Comments = (function(){
   return {
     init: function(){
-      document.querySelectorAll('.add_comment_btn').forEach(function(btn){
-        btn.addEventListener('click', function(event){
+      var addList = document.querySelectorAll('.add_comment_btn');
+      for(var i=0; i<addList.length; i++){
+        var addBtn = addList[i];
+        addBtn.addEventListener('click', function(event){
           event.preventDefault();
           event.target.closest('.comment_list_wrapper')
             .querySelector('.new_comment')
             .classList.remove('hidden');
         });
-      });
+      };
 
-      document.querySelectorAll('.edit_comment_btn').forEach(function(btn){
-        btn.addEventListener('click', function(event){
+      var editList = document.querySelectorAll('.edit_comment_btn');
+      for(var j=0; j<editList.length; j++){
+        var editBtn = editList[j];
+        editBtn.addEventListener('click', function(event){
           event.preventDefault();
-          let comment_body = event.target.closest('.body');
+          var comment_body = event.target.closest('.body');
           comment_body.querySelector('.show').classList.add('hidden');
           comment_body.querySelector('.edit').classList.remove('hidden');
         });
-      });
+      };
 
-      document.querySelectorAll('.cancel_edit_comment_btn').forEach(function(btn){
-        btn.addEventListener('click', function(event){
+      var cancelList = document.querySelectorAll('.cancel_edit_comment_btn');
+      for(var k=0; k<editList.length; k++){
+        var cancelBtn = cancelList[k];
+        cancelBtn.addEventListener('click', function(event){
           event.preventDefault();
-          let comment_body = event.target.closest('.body');
+          var comment_body = event.target.closest('.body');
           comment_body.querySelector('.show').classList.remove('hidden');
           comment_body.querySelector('.edit').classList.add('hidden');
         });
-      });
+      };
     }
   };
 })();
