5a8334d9b565c54bbca7ab4b7ff3ff6f291e0e5c
Domhnall <domhnall.murphy@examtime.com>
2018-10-27 09:52:01 +0100

    Add really basic specs for the Marketplace::ListingsController
    TODO Will need to extend this once we firm up on behaviour.

---
+++ spec/controllers/marketplace/listings_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Marketplace::ListingsController do
+  include Factories::Marketplace
+
+  before :all do
+    @user = create_user(email: "stuart@baggs.com")
+  end
+
+  describe "#index" do
+    describe "unauthenticated user" do
+      it "should redirect user to the sign in path" do
+        get :index
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "authenticated user" do
+      before :each do
+        sign_in @user
+      end
+
+      it "should return a successful response" do
+        get :index
+        expect(response.status).to eq 200
+      end
+
+      it "should render the :index template" do
+        get :index
+        expect(response.body).to render_template :index
+      end
+    end
+  end
+end
