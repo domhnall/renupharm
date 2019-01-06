0c28a391cb613d7c6591742e19f0748bba1d6e86
Domhnall <domhnall.murphy@examtime.com>
2019-01-06 09:20:19 +0000

    Change how we mount vue component
    Mounting a wrapper round the page seemed to be interfering with the
    non-Vue truncate behaviour.

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -16,9 +16,12 @@ Vue.component('product-select', ProductSelect);
 Vue.component('product', Product);
 
 document.addEventListener('turbolinks:load', () => {
-  const app = new Vue({
-    el: '[data-behaviour="vue"]'
-  });
+
+  if(document.querySelector('[data-behaviour="vue"]')){
+    const app = new Vue({
+      el: '[data-behaviour="vue"]'
+    });
+  }
 
   flatpickr("#marketplace_listing_expiry", {});
 });
