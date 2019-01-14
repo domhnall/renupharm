352899df4a941c11e4affa937acd46f89efe808b
Domhnall <domhnall.murphy@examtime.com>
2019-01-14 15:26:11 +0000

    Changing indentation on the main.js - just because.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,46 +1,46 @@
 "use strict";
 (function($){
   document.addEventListener("turbolinks:load", function() {
-      /*---------------------------------------------*
-       * Mobile menu
+    /*---------------------------------------------*
+     * Mobile menu
        ---------------------------------------------*/
-      $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
-          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-              if (target.length) {
-                  $('html,body').animate({
-                      scrollTop: (target.offset().top - 50)
-                  }, 1000);
-                  if ($('.navbar-toggle').css('display') != 'none') {
-                      $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                  }
-                  return false;
-              }
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: (target.offset().top - 50)
+          }, 1000);
+          if ($('.navbar-toggle').css('display') != 'none') {
+            $(this).parents('.container').find(".navbar-toggle").trigger("click");
           }
-      });
-      
-      
-
-  //---------------------------------------------
-  // Scroll Up 
-  //---------------------------------------------
-
-      $(window).scroll(function () {
-          if ($(this).scrollTop() > 600) {
-              $('.scrollup').fadeIn('slow');
-          } else {
-              $('.scrollup').fadeOut('slow');
-          }
-      });
-      $('.scrollup').click(function () {
-          $("html, body").animate({scrollTop: 0}, 1000);
           return false;
-      });
+        }
+      }
+    });
+
+
+
+    //---------------------------------------------
+    // Scroll Up 
+    //---------------------------------------------
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 600) {
+        $('.scrollup').fadeIn('slow');
+      } else {
+        $('.scrollup').fadeOut('slow');
+      }
+    });
+    $('.scrollup').click(function () {
+      $("html, body").animate({scrollTop: 0}, 1000);
+      return false;
+    });
 
 
 
 
-      //End
+    //End
   });
 })(jQuery);
