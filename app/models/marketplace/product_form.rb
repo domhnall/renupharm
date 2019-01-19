b8a3e4a41155e87385ce6df84b83c5cdfa0e3858
Domhnall <domhnall.murphy@examtime.com>
2019-01-19 16:12:27 +0000

    Refining the ProductForm model to handle all cases currently identified

--- app/models/marketplace/product_form.rb
+++ app/models/marketplace/product_form.rb
@@ -1,55 +1,173 @@
 class Marketplace::ProductForm
-  attr_reader :name, :strength_unit, :pack_size_unit
 
   FORMS = {
-    hard_capsules: {
-      name: "Hard capsules",
+    capsules: {
+      name: "Capsule",
       strength_unit: "mg",
-      pack_size_unit: "caps"
+      strength_required: true,
+      pack_size_unit: "caps",
+      pack_size_required: true
     },
-    soft_capsules: {
-      name: "Soft capsules",
+    tablets: {
+      name: "Tablet",
       strength_unit: "mg",
-      pack_size_unit: "caps"
+      strength_required: true,
+      pack_size_unit: "tabs",
+      pack_size_required: true
     },
     cream: {
       name: "Cream",
       strength_unit: "%",
-      pack_size_unit: "g"
+      strength_required: false,
+      pack_size_unit: "g",
+      pack_size_required: true
     },
-    pessaries: {
-      name: "Pressaries",
+    pessary: {
+      name: "Pessary",
       strength_unit: "mg",
-      pack_size_unit: "caps"
+      strength_required: false,
+      pack_size_unit: "units",
+      pack_size_required: true
     },
-    liquid: {
-      name: "Liquid",
-      strength_unit: "",
-      pack_size_unit: "ml"
+    suppository: {
+      name: "Suppository",
+      strength_unit: "mg",
+      strength_required: false,
+      pack_size_unit: "units",
+      pack_size_required: true
+    },
+    eye_ointment: {
+      name: "Eye ointment",
+      pack_size: "g",
+      pack_size_required: true
     },
     drops: {
       name: "Drops",
       strength_unit: "%",
-      pack_size_unit: "ml"
+      strength_required: false,
+      pack_size_unit: "ml",
+      pack_size_required: true
     },
     sdu: {
       name: "Single-dosage unit",
       strength_unit: "%",
-      pack_size_unit: "dose"
+      strength_required: false,
+      pack_size_unit: "doses"
     },
-    flexpen: {
-      name: "Flexpen",
+    oral_liquid: {
+      name: "Oral liquid",
+      pack_size_unit: "ml",
+      pack_size_required: true
+    },
+    syrup: {
+      name: "Syrup",
+      pack_size_unit: "ml",
+      pack_size_required: true
+    },
+    topical_liquid: {
+      name: "Topical liquid/ointment",
+      pack_size: "ml",
+      pack_size_required: true
+    },
+    inhalation_powder: {
+      name: "Inhalation powder",
+      strength_unit: "mcg",
+      strength_required: true,
+      pack_size_unit: "doses",
+      pack_size_required: true
+    },
+    solution_for_inhalation: {
+      name: "Solution for inhalation",
+      strength_unit: "mcg",
+      strength_required: true,
+      pack_size_unit: "doses",
+      pack_size_required: true
+    },
+    medicated_plaster: {
+      name: "Medicated plaster",
       strength_unit: "mg",
-      pack_size_unit: "pens"
+      strength_required: true,
+      pack_size_unit: "units",
+      pack_size_required: true
+    },
+    nasal_spray: {
+      name: "Nasal spray",
+      strength_unit: "mcg",
+      strength_required: true,
+      pack_size_unit: "doses",
+      pack_size_required: true
+    },
+    nutritional_liquid: {
+      name: "Nutritional supplement (liquid)",
+      strength_unit: "kcal/ml",
+      strength_required: true,
+      pack_size_unit: "ml",
+      pack_size_required: true
+    },
+    nutritional_powder: {
+      name: "Nutritional supplement (powder)",
+      pack_size_unit: "g",
+      pack_size_required: true
+    },
+    injectable: {
+      name: "Injectable",
+      strength_unit: "mg/ml",
+      strength_required: true,
+      pack_size_unit: "units",
+      pack_size_required: true
+    },
+    leg_bag: {
+      name: "Leg bag",
+      volume_unit: "ml",
+      volume_required: true,
+      pack_size_unit: "bags",
+      pack_size_required: true
+    },
+    night_bag: {
+      name: "Night bag",
+      volume_unit: "ml",
+      volume_required: true,
+      pack_size_unit: "bags",
+      pack_size_required: true
+    },
+    catheter: {
+      name: "Catheter",
+      product_identifier_unit: "",
+      product_identifier_required: true,
+      channel_size_unit: "Ch",
+      channel_size_required: true
     }
   }.freeze
 
-  PERMITTED = FORMS.keys.map(&:to_s)
+  PERMITTED  = FORMS.keys.map(&:to_s)
+  PROPERTIES = %w(strength pack_size volume product_identifier channel_size).freeze
 
-  def initialize(name: nil, strength_unit: nil, pack_size_unit: nil)
-    @name = name
-    @strength_unit = strength_unit
-    @pack_size_unit = pack_size_unit
+  attr_reader :name, *PROPERTIES.map{ |prop| ["#{prop}_unit", "#{prop}_required"] }.flatten.map(&:to_sym)
+
+  def initialize(
+    name: nil,
+    strength_unit: nil,
+    strength_required: false,
+    pack_size_unit: nil,
+    pack_size_required: false,
+    volume_unit: nil,
+    volume_required: false,
+    product_identifier_unit: nil,
+    product_identifier_required: false,
+    channel_size_unit: nil,
+    channel_size_required: false
+  )
+    @name                        = name
+    @strength_unit               = strength_unit
+    @strength_required           = strength_required
+    @pack_size_unit              = pack_size_unit
+    @pack_size_required          = pack_size_required
+    @volume_unit                 = volume_unit
+    @volume_required             = volume_required
+    @product_identifier_unit     = product_identifier_unit
+    @product_identifier_required = product_identifier_required
+    @channel_size_unit           = channel_size_unit
+    @channel_size_required       = channel_size_required
   end
 
   def self.for(name)
@@ -60,6 +178,16 @@ class Marketplace::ProductForm
   PERMITTED.each do |name|
     define_singleton_method(name) do
       Marketplace::ProductForm.for(name)
+    end
+  end
+
+  PROPERTIES.each do |prop|
+    define_method("#{prop}_required?") do
+      !!self.send("#{prop}_required")
+    end
+
+    define_method("#{prop}_meaningful?") do
+      !!self.send("#{prop}_unit")
     end
   end
 end
--- spec/models/marketplace/product_form_spec.rb
+++ spec/models/marketplace/product_form_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 describe Marketplace::ProductForm do
   [ :name,
     :strength_unit,
-    :pack_size_unit ].each do |method|
+    :pack_size_unit,
+    :volume_unit,
+    :channel_size_unit,
+    :strength_meaningful?,
+    :strength_required?,
+    :pack_size_meaningful?,
+    :pack_size_required?,
+    :volume_meaningful?,
+    :volume_required?,
+    :product_identifier_meaningful?,
+    :product_identifier_required?,
+    :channel_size_meaningful?,
+    :channel_size_required? ].each do |method|
     it "instance should respond to the method '#{method}'" do
       expect(Marketplace::ProductForm.new).to respond_to method
     end
@@ -23,13 +35,44 @@ describe Marketplace::ProductForm do
 
       describe "when argument is a supported form" do
         it "should return a Marketplace::ProductForm instance" do
-          expect(Marketplace::ProductForm::for("hard_capsules")).to be_a Marketplace::ProductForm
+          expect(Marketplace::ProductForm::for("capsules")).to be_a Marketplace::ProductForm
         end
 
         it "should return an instance with the correct name, strength_unit and pack_size_unit" do
-          expect(Marketplace::ProductForm::for("hard_capsules").name).to eq "Hard capsules"
-          expect(Marketplace::ProductForm::for("hard_capsules").strength_unit).to eq "mg"
-          expect(Marketplace::ProductForm::for("hard_capsules").pack_size_unit).to eq "caps"
+          expect(Marketplace::ProductForm::for("capsules").name).to eq "Capsule"
+          expect(Marketplace::ProductForm::for("capsules").strength_unit).to eq "mg"
+          expect(Marketplace::ProductForm::for("capsules").pack_size_unit).to eq "caps"
+        end
+      end
+    end
+  end
+
+  describe "instance method" do
+    #%w(strength pack_size volume channel_size).each do |property|
+    %w(strength).each do |property|
+      describe "##{property}_required?" do
+        it "should return true for a product form which mandates that #{property} to be captured" do
+          prod = Marketplace::ProductForm.new({"name"=>"gel", "#{property}_unit"=>"mg", "#{property}_required"=>true}.symbolize_keys)
+          expect(prod.send("#{property}_required?")).to be_truthy
+        end
+
+        it "should return false for a product form which does not require #{property} to be captured" do
+          prod = Marketplace::ProductForm.new({"name"=>"gel", "#{property}_unit"=>"mg", "#{property}_required"=>false}.symbolize_keys)
+          expect(prod.send("#{property}_required?")).to be_falsey
+        end
+      end
+
+      describe "##{property}_meaningful?" do
+        it "should return true for a product form which mandates the #{property}_unit" do
+          prod = Marketplace::ProductForm.new({"name"=>"gel", "#{property}_unit"=>"mg"}.symbolize_keys)
+          expect(prod.send("#{property}_meaningful?")).to be_truthy
+        end
+
+        it "should return false for a product form which does not mandate the #{property}_unit" do
+          prod = Marketplace::ProductForm.new({"name"=>"gel", "#{property}_unit"=>nil}.symbolize_keys)
+          expect(prod.send("#{property}_meaningful?")).to be_falsey
+          prod = Marketplace::ProductForm.new({"name"=>"gel"}.symbolize_keys)
+          expect(prod.send("#{property}_meaningful?")).to be_falsey
         end
       end
     end
