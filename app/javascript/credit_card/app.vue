b06a0168781d727051331fa220ae43a346b61b5b
Domhnall <domhnall.murphy@examtime.com>
2019-02-21 21:59:53 +0000

    Adding a Vue element for the credit card

---
+++ app/javascript/credit_card/app.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="card credit_card">
+    <h3 class="default" v-if="is_default">Default</h3>
+    <div class="brand">
+      <img height="41" width="65" :alt="brand" :src="brand_image">
+    </div>
+    <div class="number">
+      **** **** **** {{ number }}
+    </div>
+    <div class="expiry">
+      <div class="label">Expiry</div>
+      <div class="value">{{ expiry_month }}/{{ expiry_year }}</div>
+    </div>
+    <div class="holder text-truncate" :title="email">
+      {{ email }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      brand: {
+        type: String,
+        default: 'visa'
+      },
+      number: {
+        type: String,
+        default: 'XXXX'
+      },
+      expiry_month: {
+        type: String,
+        default: 'mm',
+      },
+      expiry_year: {
+        type: String,
+        default: 'yyyy',
+      },
+      email: {
+        type: String,
+        default: 'joe@example.com'
+      },
+      is_default: {
+        type: Boolean,
+        default: false,
+      },
+      brand_image: {
+        type: String,
+        default: ""
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .credit_card {
+    border: 1px solid black;
+    border-radius: 15px;
+    position: relative;
+    padding: 10px;
+    width: 250px !important;
+    height: 150px;
+  }
+  .credit_card .brand {
+    text-align: right;
+  }
+  .credit_card .number {
+    width: 100%;
+    margin: 10px auto;
+    font-weight: bold;
+    font-size: 120%;
+    text-align: center;
+  }
+  .credit_card .expiry {
+    width: 100%;
+    text-align: left;
+  }
+  .credit_card .holder {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    margin-top: 10px;
+  }
+
+  .credit_card h3.default {
+    display: inline-block;
+    width: 70px;
+    margin-top: -20px;
+    margin-left: 10px;
+    padding: 0 5px;
+    color: #8ad4ee;
+    text-transform: uppercase;
+    font-size: 90%;
+    font-weight: bold;
+    background: white;
+  }
+</style>
--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -9,13 +9,16 @@ import '../utils';
 import '../legal';
 import '../product_edit';
 import '../pharmacy_profile';
+import '../cart';
 
 import ProductSelect from '../product_select/app.vue';
 import Product from '../product_select/product.vue';
+import CreditCard from '../credit_card/app.vue';
 
 Vue.use(TurbolinksAdapter);
 Vue.component('product-select', ProductSelect);
 Vue.component('product', Product);
+Vue.component('credit-card', CreditCard);
 
 document.addEventListener('turbolinks:load', () => {
 
@@ -23,6 +26,9 @@ document.addEventListener('turbolinks:load', () => {
     const app = new Vue({
       el: '[data-behaviour="vue"]'
     });
+    let e = document.createEvent('HTMLEvents');
+    e.initEvent("renupharm:vue:initialized", false, true);
+    document.dispatchEvent(e);
   }
 
   flatpickr("#marketplace_listing_expiry", {});
