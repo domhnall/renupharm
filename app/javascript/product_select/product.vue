f0baecce409009a18b8a6f373a31c6d58ffb084e
Domhnall <domhnall.murphy@examtime.com>
2019-03-18 20:19:38 +0000

    Product and listing style changes for mobile

--- app/javascript/product_select/product.vue
+++ app/javascript/product_select/product.vue
@@ -1,21 +1,20 @@
 <template>
-<div class="card product">
+<div class="card product p-2">
   <h2 class="selected" v-if="selected">Selected</h2>
   <div class="row">
-    <div class="col-5">
+    <div class="col-12 col-sm-7 offset-sm-5">
+      <div class="name">
+        {{ name }}
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-5">
       <div v-if="image_urls.length>0" class="product_images">
         <img class="d-block w-100 img-product" :src="image_urls[0]">
       </div>
     </div>
 
-    <div class="col-7">
-      <div class="name">
-        {{ name }}
-      </div>
-      <div class="active_ingredient">
-        <span class="label">Active ingredient:</span>
-        <span class="value">{{ active_ingredient }}</span>
-      </div>
+    <div class="col-12 col-sm-7">
       <div class="product_form">
         <span class="label">Form:</span>
         <span class="value">{{ product_form_name }}</span>
@@ -27,6 +26,10 @@
       <div class="pack_size">
         <span class="label">Pack size:</span>
         <span class="value">{{ display_pack_size }}</span>
+      </div>
+      <div class="active_ingredient">
+        <span class="label">Active ingredient:</span>
+        <span class="value">{{ active_ingredient }}</span>
       </div>
       <div class="manufacturer">
         <span class="label">Manufacturer:</span>
@@ -72,7 +75,7 @@ export default {
 }
 
 .card.product .label {
-  min-width: 140px;
+  min-width: 50px;
 }
 
 </style>
